8d2eff4131ef891a3e3f61c4969d9c126b11a987
Natasha <nhodg2@gmail.com>
2023-08-07 20:46:12 -0500

    Update Recipe components

--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,9 +1,14 @@
+import React from 'react';
+
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
+
+
 
 function RateARecipe(props) {
   function GiveRating() {
     return <h3>{stars[props.rating - 1]}</h3>
   }
+
   return props.rating <= 5 && props.rating >= 1 ? <GiveRating /> : null;
 }
 
